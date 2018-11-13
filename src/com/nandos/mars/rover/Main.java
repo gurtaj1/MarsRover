cc4bda251223881971d8f156480f74e5d3aaeed2
Gurtaj <gurtaj@gmail.com>
2018-11-13 00:29:44 +0000

    reintroduce initial two rovers, and make third one with ability to traverse whole grid so long as it does not intercept other rovers

--- src/com/nandos/mars/rover/Main.java
+++ src/com/nandos/mars/rover/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Plateau plateau = new Plateau(new Position(0,0),new Position(5,5));
-		Rover rover = new Rover(plateau, new Position(0,0),'N');
-		rover.moveRover("MMMMMRMRMMMMMLMLMMMMMRMRMMMMMLMLMMMMMRMRMMMMM");
+		Rover rover = new Rover(plateau, new Position(1,2),'N');
+		rover.moveRover("LMLMLMLMM");
+		System.out.println(rover.getPosition());
+		
+		rover = new Rover(plateau, new Position(3,3),'E');
+		rover.moveRover("MMRMMRMRRM");
+		System.out.println(rover.getPosition());
+
+		rover = new Rover(plateau, new Position(5,0),'W');
+		rover.moveRover("MMMMMRRMMLMRMLMRMLLMMRMMLMMLMRRMMRMMMMMRMRMM");
 		System.out.println(rover.getPosition());
 	}
 
