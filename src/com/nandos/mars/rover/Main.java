d8ba9f7122a0149207c6db04a542306372da4c4a
Gurtaj <gurtaj@gmail.com>
2018-11-13 00:20:40 +0000

    remove first two instances of rover (assuming overs cannot cross over each other's current grid positions

--- src/com/nandos/mars/rover/Main.java
+++ src/com/nandos/mars/rover/Main.java
@@ -4,15 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Plateau plateau = new Plateau(new Position(0,0),new Position(5,5));
-		Rover rover = new Rover(plateau, new Position(1,2),'N');
-		rover.moveRover("LMLMLMLMM");
-		System.out.println(rover.getPosition());
-		
-		rover = new Rover(plateau, new Position(3,3),'E');
-		rover.moveRover("MMRMMRMRRM");
-		System.out.println(rover.getPosition());
-
-		rover = new Rover(plateau, new Position(0,0),'N');
+		Rover rover = new Rover(plateau, new Position(0,0),'N');
 		rover.moveRover("MMMMMRMRMMMMMLMLMMMMMRMRMMMMMLMLMMMMMRMRMMMMM");
 		System.out.println(rover.getPosition());
 	}
